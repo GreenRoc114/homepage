b59df5100d3e058765f8adebcfe25abf39ea0486
GreenRoc114 <168276820+GreenRoc114@users.noreply.github.com>
2025-06-07 16:58:57 +0800

    Update home.vue

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -35,73 +35,27 @@ const appConfig = useAppConfig()
                 <ZLink to="http://www.ahhf45.com">
                     安徽合肥第45中学
                 </ZLink>初三学生，Windows 11 和 Arch
-                Linux 用户。
+                Linux 用户。擅长Ai、Fw、Fl、Br、Ae、Pr、Id、PS等软件的安装与卸载，精通CSS、JavaScript、PHP、C、C＋＋、java、Ruby、Perl、Lisp、python、Objective-C、ActionScript等单词的拼写，熟悉windows、Linux、Mac、Android、IOS等系统的开关机。
             </p>
         </ZField>
 
         <ZField label="近期活动">
             <p>
-                2024 年 7 月完善<ZLink to="https://wiki.cooo.site/">
-                    西邮Wiki
-                </ZLink>，8 月使用 Nuxt Content 重构了<ZLink to="https://blog.zhilu.cyou/">
-                    个人博客
-                </ZLink>，2025 年 3 月重构了西邮 Linux 兴趣小组博客后端，近期正在寻找前端实习。
+                2024年8月购买ahhf45.top域名并有了建站想法，在暑假得以实现
+                2025年3月第一次使用了<Zlink to="https://https://github.com/saicaca/fuwari">
+                    fuwari
+                </Zlink>构建了静态博客并发表了几篇文章，后因手抖删掉了仓库并忘了备份顺势换了个主题
             </p>
         </ZField>
 
         <ZField label="关于主页">
             <p>
-                自2024年6月1日开始重构，目前缓慢修缮中。
-            </p>
-            <p>
-                感谢<ZBadge link="https://github.com/KazariEX">
-                    KazariEX
-                </ZBadge>
-                <ZBadge link="https://github.com/isYangs">
-                    isYangs
-                </ZBadge>等众多朋友为个人主页提供帮助。<br>
-                主页开源在<ZLink to="https://github.com/L33Z22L11/homepage-v5" icon="ph:github-logo-duotone">
+                Fork自开源项目<ZLink to="https://github.com/L33Z22L11/homepage-v5" icon="ph:github-logo-duotone">
                     L33Z22L11/homepage-v5
-                </ZLink>上。
+                </ZLink>。
+                
             </p>
-            <p>
-                欢迎使用、参考样式：<br>
-                <ZBadge
-                    img="https://image.m-c.top/?/images/2024/07/21/iyt3mhQCDe/b_a3f6e95501bcc4ce64c19c63a1211bcd.png"
-                    link="https://oio.mckfs.com/"
-                    text="Oiolosse MC服"
-                />
-                <ZBadge
-                    img="https://bu.dusays.com/2024/07/13/66929d307adb9.png"
-                    link="https://henrywhu.cn/"
-                    text="Henry Moreau"
-                />
-                <ZBadge
-                    img="https://api-space.tnxg.top/avatar?s=qq"
-                    link="https://tnxg.top/"
-                    text="天翔TNXG"
-                />
-                <ZBadge
-                    img="https://ykc.im/icon.png"
-                    link="https://ykc.im/"
-                    text="York Chou"
-                />
-                <ZBadge
-                    img="https://www.xxfer.cn/icon.png"
-                    link="https://www.xxfer.cn/"
-                    text="小李同学"
-                />
-                <ZBadge
-                    img="https://q1.qlogo.cn/g?b=qq&nk=1043865083&s=2"
-                    link="https://www.xlenco.top/"
-                    text="Xlenco"
-                />
-                <ZBadge
-                    img="https://www.mugzx.top/icon.png"
-                    link="https://www.mugzx.top/"
-                    text="Mugzx"
-                />
-            </p>
+          
         </ZField>
     </div>
 </template>
